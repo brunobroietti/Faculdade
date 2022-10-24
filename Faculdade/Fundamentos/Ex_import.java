8d4f73a5c36f198ef7f853b2017903e154c2d5cd
pitbull_lavajato <101436053+brunobroietti@users.noreply.github.com>
2022-10-24 00:01:17 -0300

    Create Ex_import.java

---
+++ Faculdade/Fundamentos/Ex_import.java
@@ -0,0 +1,10 @@
+package Facul;
+
+import java.util.Date;
+
+public class Ex_Import {
+    public static void main(String[] args) {
+        Date data = new Date();
+        System.out.println("Hoje é dia: "+data);
+    }
+}
