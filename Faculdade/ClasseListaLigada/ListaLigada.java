eadb5e2cd9adc213c7b7feb2ed3f121162581617
pitbull_lavajato <101436053+brunobroietti@users.noreply.github.com>
2022-10-26 21:31:36 -0300

    Create ListaLigada.java

---
+++ Faculdade/ClasseListaLigada/ListaLigada.java
@@ -0,0 +1,70 @@
+package ClasseListaLigada;
+
+public class ListaLigada<T> {
+
+    private NoPetri<T> primeiro;
+    private NoPetri<T> ult;
+    private int tamanho;
+
+    public ListaLigada() {
+        ult = null;
+        tamanho = 0;
+    }
+
+    public int getTamanho() {
+        return tamanho;
+    }
+
+    public void adicionaInicio(T conteudo) {
+        NoPetri<T> novoNo = new NoPetri<T>(conteudo, null);
+        primeiro = novoNo;
+        tamanho++;
+    }
+
+    public void adicionaFinal(T conteudo) {
+        NoPetri<T> novoNo = new NoPetri<T>(conteudo, null);
+        if (tamanho == 0) {
+            primeiro = novoNo;
+        } else {
+            NoPetri<T> atual = primeiro;
+            while (atual.getProximo() != null) {
+                atual = atual.getProximo();
+            }
+            novoNo.setAntes(atual);
+
+            atual.setProximo(novoNo);
+            ult = novoNo;
+        }
+        tamanho++;
+    }
+
+    public void Mostrar() {
+        if (tamanho != 0) {
+            NoPetri<T> atual = primeiro;
+            int cont = 0;
+            while (atual.getProximo() != null) {
+                System.out.println("O dado do Nó na posição " + cont + " é : " + atual.getConteudo());
+                atual = atual.getProximo();
+                cont++;
+            }
+            System.out.println("O dado do Nó na posição " + cont + " é: " + atual.getConteudo());
+        } else {
+            System.out.println("Não há elemento na lista!!!");
+        }
+    }
+
+    public void Decrescente() {
+        if (tamanho != 0) {
+            NoPetri<T> atual = ult;
+            int cont = 0;
+            while (atual.getAntes() != null) {
+                System.out.println("O dado do Nó na posição " + cont + " é: " + atual.getConteudo());
+                atual = atual.getAntes();
+                cont++;
+            }
+            System.out.println("O dado do Nó na posição " + cont + " é: " + atual.getConteudo());
+        } else {
+            System.out.println("Não há elemento na lista!!!");
+        }
+    }
+}
