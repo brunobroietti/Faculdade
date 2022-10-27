4e4128e4a563fe09ae23c92810fc0cc30f109181
pitbull_lavajato <101436053+brunobroietti@users.noreply.github.com>
2022-10-27 19:33:14 -0300

    Update ExecNoPetri.java

--- Faculdade/ClasseListaLigada/ExecNoPetri.java
+++ Faculdade/ClasseListaLigada/ExecNoPetri.java
@@ -6,9 +6,13 @@ public class ExecNoPetri {
     public static void main(String[] args) {
         ListaLigada listaOn = new ListaLigada<>();
 
-        listaOn.adicionaInicio(100);
-        listaOn.adicionaFinal(200);
-        listaOn.adicionaFinal(300);
+        listaOn.adicionaInicio(1);
+        listaOn.adicionaFinal(2);
+        listaOn.adicionaFinal(3);
+        listaOn.adicionaInicio(4);
+        listaOn.adicionaFinal(5);
+        listaOn.adicionaInicio(6);
+        listaOn.adicionaFinal(7);
         listaOn.Mostrar();
         System.out.println();
         listaOn.Decrescente();
