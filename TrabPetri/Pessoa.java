aabed3db35cf48a900a02d07ac7d69a01c52e933
pitbull_lavajato <101436053+brunobroietti@users.noreply.github.com>
2022-11-02 20:16:42 -0300

    Create Pessoa.java

---
+++ TrabPetri/Pessoa.java
@@ -0,0 +1,33 @@
+package TrabPetri;
+
+public class Pessoa {
+    String nomeTitular;
+    int cpf;
+    int idade;
+
+    public Pessoa(String nome, int cpf, int idade) {
+        this.nomeTitular = nome;
+        this.cpf = cpf;
+        this.idade = idade;
+    }
+
+    public String getNomeTitular() {
+        return nomeTitular;
+    }
+
+    public void setNomeTitular(String nomeTitular) {
+        this.nomeTitular = nomeTitular;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public int getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(int cpf) {
+        this.cpf = cpf;
+    }
+}
