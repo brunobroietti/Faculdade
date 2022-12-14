7ced61fba1b81a085477eaca66e36c24e873ffa5
pitbull_lavajato <101436053+brunobroietti@users.noreply.github.com>
2022-12-14 01:41:57 -0300

    Create Break.java

---
+++ Faculdade/Controle_Estruturas/Break.java
@@ -0,0 +1,16 @@
+package Controle_Estruturas;
+
+public class Break {
+    public static void main(String[] args) {
+        int i;
+
+        for (i = 0; i < 10; i++) {
+
+            if (i == 5) {
+                break;
+            }
+            System.out.println(i);
+        }
+        System.out.println("Fim!");
+    }
+}
