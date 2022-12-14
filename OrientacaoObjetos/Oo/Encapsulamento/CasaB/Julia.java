a4822fb6dc4321a12a9c7c98f2fd80dd04ac8e15
pitbull_lavajato <101436053+brunobroietti@users.noreply.github.com>
2022-12-14 01:34:47 -0300

    Create Julia.java

---
+++ OrientacaoObjetos/Oo/Encapsulamento/CasaB/Julia.java
@@ -0,0 +1,15 @@
+package Oo.Encapsulamento.CasaB;
+
+import Oo.Encapsulamento.CasaA.Ana;
+
+public class Julia {
+
+    void testeAcessos() {
+        Ana sogra = new Ana();
+
+        // System.out.println(sogra.segredo);
+        // System.out.println(sogra.facoDentroDeCasa);
+        // System.out.println(sogra.formaDeFalar);
+        System.out.println(sogra.todosSabem);
+    }
+}
